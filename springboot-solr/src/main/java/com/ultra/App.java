5eeae2022ea0426b046c3e7b70c1f8dacfee782f
ZXMJ <1343421239@qq.com>
2019-08-16 19:53:26 +0800

    solr

---
+++ springboot-solr/src/main/java/com/ultra/App.java
@@ -0,0 +1,13 @@
+package com.ultra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
